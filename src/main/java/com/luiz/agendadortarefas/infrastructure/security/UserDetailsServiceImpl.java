e6449eb2369bf94e6281718de9e852db1019042d
Luiz <luizgsch1@gmail.com>
2026-02-18 14:20:18 -0300

    adicionadas configurações de segurança

---
+++ src/main/java/com/luiz/agendadortarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.luiz.agendadortarefas.infrastructure.security;
+
+import com.luiz.agendadortarefas.business.dto.UsuarioDTO;
+import com.luiz.agendadortarefas.infrastructure.client.UsuarioClient;
+import com.luiz.usuario.infrastructure.entity.Usuario;
+import com.luiz.usuario.infrastructure.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UsuarioClient client;
+
+
+
+
+    public UserDetails carregaDadosUsuario(String email, String token){
+
+        UsuarioDTO usuarioDTO = client.buscaUsuarioPorEmail(email, token);
+        return User
+                .withUsername(usuarioDTO.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDTO.getSenha()) // Define a senha do usuário
+                .build();
+    }
+}
